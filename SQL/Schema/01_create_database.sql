80f560d1441fe4bd7b7da63da32361e2926db0f8
prafulkatariya70-cmyk <prafulkatariya70@gmail.com>
2026-07-12 17:38:20 +0530

    Add SQL schema scripts

---
+++ SQL/Schema/01_create_database.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE InvestmentPortfolioAnalytics;
+GO
+
+USE InvestmentPortfolioAnalytics;
+GO
---
+++ SQL/Schema/02_create_schemas.sql
@@ -0,0 +1,14 @@
+USE InvestmentPortfolioAnalytics;
+GO
+
+CREATE SCHEMA client;
+GO
+
+CREATE SCHEMA portfolio;
+GO
+
+CREATE SCHEMA market;
+GO
+
+CREATE SCHEMA analytics;
+GO
